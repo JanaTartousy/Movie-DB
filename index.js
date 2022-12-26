287b848f1cefa4b5fb941b3667e2ab98100cf18b
JanaTartousy <tartousyjana@gmail.com>
2022-12-26 23:42:24 +0200

    step 5

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ app.listen( port ,
 
 app.get('/test',(req,res) => {
 res.status(200).json({ message: "ok" });
-})
+});
 app.get('/time',(req,res) => {
 const date = new Date();
 const hours = date.getHours();
 const minutes = date.getMinutes();
 res.status(200).json({ message: `${hours} : ${minutes}` });
-})
+});
 app.get('/hello',(req,res) => {
 const ID= "";
 res.send({status:200, message:"Hello, <ID>"});
-})
+});
 app.get("/search",(req,res)=>{
     console.log(req.query.s)
     if(typeof req.query.s =="undefined" || req.query.s === "") 
@@ -32,4 +32,21 @@ app.get("/search",(req,res)=>{
     }else {
         res.send( {status:200, message:"ok", data:search})
     }
-})
+});
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }];
+app.get('/movies/create',(req,res)=>{
+    res.status(200).json({message:"done" });
+});
+app.get('/movies/read',(req,res)=>{
+    res.status(200).json({ data:movies });
+});
+app.get('/movies/update',(req,res)=>{
+    res.status(200).json({message:"updated" });
+});
+app.get('/movies/delete',(req,res)=>{
+    res.status(200).json({ message:"deleted" });
+});
