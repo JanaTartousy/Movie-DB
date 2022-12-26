7f5b82724a76a5e9a8d179bb43ac95dc4a30726a
JanaTartousy <tartousyjana@gmail.com>
2022-12-26 19:21:04 +0200

    step 3

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+const { response } = require('express');
 const express = require( 'express' )
 const app     = express()
 const port    = process.env.PORT || 3000
+
 app.get( '/' ,(req, res) => {
 res.type( 'text/plain' )
 res.send( 'ok' )
 })
-app.get( '/about', ( req, res ) => {
-res.type( 'text/plain' )
-res.send( 'Listen About')
-})
-app.use( ( req, res ) => {
-res.type( 'text/plain' )
-res.status( 404 )
-res.send('404 Not found ')
-})
+
 app.listen( port ,
 () => console.log(`Listen is on Port at http://localhost:${ port } Ctrl + C to Stop `) ) 
+
+
+app.get('/test',(req,res) => {
+res.status(200).json({ message: "ok" });
+})
+app.get('/time',(req,res) => {
+const date = new Date();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+res.status(200).json({ message: `${hours} : ${minutes}` });
+})
