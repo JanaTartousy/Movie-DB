e7e8b9dbb846507633cec287e2ca72b7f5d0b9e0
JanaTartousy <tartousyjana@gmail.com>
2022-12-27 13:57:43 +0200

    step 6

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ res.send( 'ok' )
 app.listen( port ,
 () => console.log(`Listen is on Port at http://localhost:${ port } Ctrl + C to Stop `) ) 
 
-
+//simple API
 app.get('/test',(req,res) => {
 res.status(200).json({ message: "ok" });
 });
@@ -21,6 +21,7 @@ const hours = date.getHours();
 const minutes = date.getMinutes();
 res.status(200).json({ message: `${hours} : ${minutes}` });
 });
+//complicate API
 app.get('/hello',(req,res) => {
 const ID= "";
 res.send({status:200, message:"Hello, <ID>"});
@@ -33,6 +34,7 @@ app.get("/search",(req,res)=>{
         res.send( {status:200, message:"ok", data:search})
     }
 });
+//basis for CRUD
 const movies = [
     { title: 'Jaws', year: 1975, rating: 8 },
     { title: 'Avatar', year: 2009, rating: 7.8 },
@@ -50,3 +52,24 @@ app.get('/movies/update',(req,res)=>{
 app.get('/movies/delete',(req,res)=>{
     res.status(200).json({ message:"deleted" });
 });
+//search
+app.get('/movies/read/by-date', (req,res)=>{
+    const moviesbydate= movies.sort((a,b)=>a.year - b.year);
+    res.send( {status:200, data:moviesbydate});
+});
+app.get('/movies/read/by-rating', (req,res)=>{
+    const moviesbyrate= movies.sort((a, b)=> a.rating - b.rating);
+    res.send( {status:200, data:moviesbyrate});
+});
+app.get('/movies/read/by-title', (req,res)=>{
+    const moviesbytitle= movies.sort((a, b)=>{
+        if (a.title< b.title){
+            return - 1;
+        }
+        if (a.title> b.title){
+            return 1;
+        }
+        return 0;
+    })
+        res.send( {status:200, data:moviesbytitle});
+    });
